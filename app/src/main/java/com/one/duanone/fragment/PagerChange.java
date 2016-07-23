7451aaf4fc5db92573a1fcaef82a4d20965b84de
chengquan1990 <1321390250@qq.com>
2016-07-23 19:18:43 +0800

    FCQ

--- app/src/main/java/com/one/duanone/fragment/PagerChange.java
+++ app/src/main/java/com/one/duanone/fragment/PagerChange.java
@@ -4,6 +4,8 @@ package com.one.duanone.fragment;
  * 本文档由：成全 于：2016/7/23 创建
  */
 public interface PagerChange {
-        void onPageChange(int pager);
-        int a = 0;
+    void onPageChange(int pager);
+
+    int a = 0;
+    int b = 0;
 }
