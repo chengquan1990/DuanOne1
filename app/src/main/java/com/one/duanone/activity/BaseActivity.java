879bd5e614a68f8b3a1da8414f589932601fdfd9
chengquan1990 <1321390250@qq.com>
2016-07-25 14:44:17 +0800

    FCQ

--- app/src/main/java/com/one/duanone/activity/BaseActivity.java
+++ app/src/main/java/com/one/duanone/activity/BaseActivity.java
@@ -1,5 +1,7 @@
 package com.one.duanone.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
 /**
@@ -7,6 +9,8 @@ import android.support.v4.app.FragmentActivity;
  */
 public class BaseActivity extends FragmentActivity {
 
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
